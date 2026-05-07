1b96b4d2dcc8b855f0c17b6fc5804eb02a963480
subvrs <subverse.business@gmail.com>
2026-05-07 20:52:22 +0200

    Update supabase.js

--- subvrs/lib/supabase.js
+++ subvrs/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://omsbaccxhusnfpusafgi.supabase.co';
-const supabaseKey = 'sb_publishable_K1pUFtnysyMGFZr0BVeFUA_Yzzs7XTQaJTs0nw';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tc2JhY2N4aHVzbmZwdXNhZmdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNjcwNTksImV4cCI6MjA5Mzc0MzA1OX0.j8L8i4lU-FdTC0sjI6L2Z79_w2FBMtrGIuberfyJCfY';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
