f4a9bf063512c79f6418af80acd787ceda57f160
subvrs <subverse.business@gmail.com>
2026-05-10 19:06:39 +0200

    Add files via upload

--- subvrs/pages/_document.js
+++ subvrs/pages/_document.js
@@ -9,8 +9,12 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MWMDVJRV');` }} />
       </Head>
       <body>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MWMDVJRV" height="0" width="0" style={{ display: 'none', visibility: 'hidden' }} />
+        </noscript>
         <Main />
         <NextScript />
       </body>
